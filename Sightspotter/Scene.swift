5d66361f6bfd8a39823b304dad21986bcce78be0
Matthew Jacome <MXJacome@fullsail.edu>
2017-07-08 21:35:47 -0400

    Targets appear

--- Sightspotter/Scene.swift
+++ Sightspotter/Scene.swift
@@ -8,11 +8,70 @@
 
 import SpriteKit
 import ARKit
+import GameplayKit
 
 class Scene: SKScene {
     
+    // MARK: - Properties
+    let remainingLabel = SKLabelNode()
+    var timer: Timer?
+    var targetsCreated = 0
+    var targetCount = 0 {
+        didSet {
+            remainingLabel.text = "Remaining: \(targetCount)"
+        }
+    }
+    // MARK: - Functions
+    func createTarget() {
+        // Create targets
+        if targetsCreated == 20 {
+            timer?.invalidate()
+            timer = nil
+            return
+        }
+        
+        targetsCreated += 1
+        targetCount += 1
+        
+        // find the scene view we are drawing into
+        guard let sceneView = self.view as? ARSKView else { return }
+        
+        // get access to a random number generator
+        let random = GKRandomSource.sharedRandom()
+        
+        // create a random X rotation
+        let xRotation = simd_float4x4(SCNMatrix4MakeRotation(Float.pi * 2 * random.nextUniform(), 1, 0, 0))
+        
+        // create a random Y rotation
+        let yRotation = simd_float4x4(SCNMatrix4MakeRotation(Float.pi * 2 * random.nextUniform(), 0, 1, 0))
+        
+        // combine them together
+        let rotation = simd_mul(xRotation, yRotation)
+        
+        // move forward 1.5 meters into the screen
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -1.5
+        
+        // combinee that with our rotation
+        let transform = simd_mul(rotation, translation)
+        
+        // create an anchor at the finished position
+        let anchor = ARAnchor(transform: transform)
+        sceneView.session.add(anchor: anchor)
+    }
     override func didMove(to view: SKView) {
-        // Setup your scene here
+        // Hud
+        remainingLabel.fontSize = 36
+        remainingLabel.fontName = "AmericanTypewriter"
+        remainingLabel.color = .white
+        remainingLabel.position = CGPoint(x: 0, y: view.frame.midY - 50)
+        addChild(remainingLabel)
+        targetCount = 0
+        
+        // Timer
+        timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true) {_ in
+            self.createTarget()
+        }
     }
     
     override func update(_ currentTime: TimeInterval) {
